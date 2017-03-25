5dcb137fe07d4588aaca9395f05f94e20fec28b8
Donald Pakkies <donaldpakkies@gmail.com>
2017-03-26 01:46:46 +0200

    Add files via upload

---
+++ console/example/files/app.js
@@ -0,0 +1,8 @@
+﻿/* App Code */
+
+function Main() {
+    document.title                 = 'Untitled';
+    document.body.style.background = '#eee';
+    document.body.style.color      = 'black';
+    document.body.style.margin     = '0px';
+}
